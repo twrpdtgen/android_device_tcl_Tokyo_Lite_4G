5ad9624e247bbe9e8194b1eef6f3bb8e08e1c201
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-01-16 12:55:25 +0100

    Tokyo_Lite_4G: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: tcl Model: 5007Z Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),Tokyo_Lite_4G)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
